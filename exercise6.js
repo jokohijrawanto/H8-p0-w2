a825318d9c25bb18c8122a694450ed9e48b324aa
jokohijrawanto <biterasystem@gmail.com>
2020-03-29 22:17:45 +0700

    tambah exercise6

---
+++ exercise6.js
@@ -0,0 +1,29 @@
+/*1. Melakukan Looping Menggunakan While
+looping pertama:
+var i=2;
+while (i<=20)
+{
+   console.log(i + "  I love coding");
+   i+=2;
+}
+
+
+looping kedua:
+var i=20;
+while (i>=2)
+{
+   console.log(i +"  -"+ "  I will become fullstack developer");
+   i-=2;
+}
+*/
+
+/*
+looping pertama
+for(let i = 0; i <= 20; i++){
+    console.log(i + '  -'+" I love coding");
+}
+
+looping kedua
+for(let i = 20; i > 0; i--){
+    console.log(i + '  -'+"  will become fullstack developer");
+}
