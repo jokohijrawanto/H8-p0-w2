56de9f48b6fb54717fc7f8c65ef8f14cc06aeb3d
jokohijrawanto <biterasystem@gmail.com>
2020-03-30 02:01:35 +0700

    exercises8

---
+++ exercise8.js
@@ -0,0 +1,16 @@
+const shoutOut = () => { 
+  console.log('Halo Function!'); 
+}; 
+shoutOut(); // Prints: hello
+
+const calculateMultiply = (a, b) => a * b; 
+console.log(calculateMultiply(5, 6)); 
+
+var name = "Agus";
+var age = 30;
+var address = "Jln. Malioboro, Yogjakarta";
+var hobby = "gaming";
+
+var fullSentence =('"Nama saya '+ name+', '+' umur saya '+ age +' tahun, '+'alamat saya di '+address+', '+' dan saya punya hobby yaitu '+hobby+'!"')
+console.log(fullSentence); // Menampilkan "Nama saya Agus, umur saya 3
+
