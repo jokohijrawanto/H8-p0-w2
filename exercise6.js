0e7b9b8d7d1e24b65cee78e79458f664aecbdfcb
jokohijrawanto <biterasystem@gmail.com>
2020-03-29 23:16:23 +0700

    edit

--- exercise6.js
+++ exercise6.js
@@ -27,3 +27,50 @@ looping kedua
 for(let i = 20; i > 0; i--){
     console.log(i + '  -'+"  will become fullstack developer");
 }
+*/
+
+/*3.Angka Ganjil dan Genap
+
+var nilai = 1;
+var akhir = 100;
+  while( nilai <= akhir ){
+   		if(nilai%2==0)
+   		{
+   			console.log(nilai+" genap " );
+   		} else
+   		{
+   			console.log(nilai+" ganjil " );
+   		}
+   		nilai++;
+       }
+       
+var i=2;
+    while (i<100) {
+      if(i%3==0){
+        console.log(i + ' kelipatan 3');
+      }else {
+        console.log(i);
+      }
+      i+=2;
+}
+
+var i=5;
+    while (i<100) {
+      if(i%6==0){
+        console.log(i + ' kelipatan 6');
+      }else {
+        console.log(i);
+      }
+      i+=5;
+}
+
+var i=9;
+    while (i<100) {
+      if(i%10==0){
+        console.log(i + ' kelipatan 10');
+      }else {
+        console.log(i);
+      }
+      i+=9;
+}
+*/
